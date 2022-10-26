380ca49fc5ec3f32b142972387949df27aa59a30
Philipp K <philippkargapoloff@gmail.com>
2022-10-27 00:45:15 +0300

    added obj 1

---
+++ Objective_001/Program.cs
@@ -0,0 +1,53 @@
+﻿/*Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Через строку решать нельзя.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да*/
+
+void PalindromeTestFiveDigit(string? num)
+{
+    char[] arr;
+
+    arr = num!.ToCharArray();
+    
+    int frontCount = 0;
+    int rearCount = arr.Length - 1;
+    int centre = 2;
+    int result = 0;
+
+    if(arr.Length > 5 || arr.Length < 5)
+    {
+        Console.WriteLine(num + " is not five-digit!!!");
+    }
+    else
+    {
+        while(frontCount < centre)
+        {
+            if(arr[frontCount] == arr[rearCount]) 
+            {
+                result = 1;
+            }
+            else
+            {
+                result = 0;
+            }       
+        frontCount = frontCount+1;
+        rearCount = rearCount-1;      
+        }
+        if(result == 0)
+        {
+            Console.WriteLine("No, number " + num + " isn't a palindrome");
+        }
+        else
+            Console.WriteLine("Yes, number " + num + " is a palindrome");
+    }   
+}
+
+Console.WriteLine("Input number: ");
+string? num = Console.ReadLine();
+
+PalindromeTestFiveDigit(num);
